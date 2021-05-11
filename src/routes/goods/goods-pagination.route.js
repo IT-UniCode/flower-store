530a30028b7b5b347fc0e18c66345f499d96fe09
MaxSapeyko <maks.sapeyko@gmail.com>
2021-05-11 12:13:20 +0300

    implamented pagination

--- src/routes/goods/goods-pagination.route.js
+++ src/routes/goods/goods-pagination.route.js
@@ -1,12 +1,16 @@
 import Goods from "../../models/goods/index.js";
 
 export default function (req, res) {
-  console.log(req.body);
-
-  if (req.body.tags !== "" && req.body.type !== "") {
+  if (req.body.tags === "" && req.body.type === "") {
+    Goods.find().skip(Number(req.body.startId)).limit(Number(req.body.endId))
+      .then((goods) => {
+        res.json(goods);
+      })
+      .catch((error) => res.status(400).json(`Error ${error}`));
+  } else if (req.body.tags !== "" && req.body.type !== "") {
     Goods.find({
       $and: [{ type: req.body.type }, { tags: req.body.tags }],
-    })
+    }).skip(Number(req.body.startId)).limit(Number(req.body.endId))
       .then((goods) => {
         res.json(goods);
       })
@@ -14,13 +18,10 @@ export default function (req, res) {
   } else {
     Goods.find({
       $or: [{ type: req.body.type }, { tags: req.body.tags }],
-    })
+    }).skip(Number(req.body.startId)).limit(Number(req.body.endId))
       .then((goods) => {
         res.json(goods);
       })
       .catch((error) => res.status(400).json(`Error ${error}`));
   }
-
-  // .skip(1)
-  // .limit(3)
 }
