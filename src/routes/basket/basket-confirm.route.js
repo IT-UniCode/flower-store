7b913778fd25f1052eb10a02eb2dbe24da91bcbe
MaxSapeyko <maks.sapeyko@gmail.com>
2021-05-14 10:12:19 +0300

    added orderDate to basket Update

--- src/routes/basket/basket-confirm.route.js
+++ src/routes/basket/basket-confirm.route.js
@@ -7,7 +7,9 @@ export default function (req, res) {
     req.body
   )
     .then(() => {
-      const html = `<h1>Ваш заказ отправлен менеджеру:</h1><table style='border: 1px solid #000'><thead><tr><th>ФИО</th><th>Телефон</th><th>Адрес доставки</th><th>Сумма заказа</th><th>Статус</th></tr></thead><tbody><tr><td>${req.body.fullName}</td><td>${req.body.phone}</td><td>${req.body.address}</td><td>${req.body.price}грн.</td><td>В обработке</td></tr></tbody></table><p>Ожидайте наш менеджер свяжется с вами для подтверждения заказа </p>`;
+      const date = new Date(req.body.orderDate).toLocaleDateString();
+
+      const html = `<h1>Ваш заказ отправлен менеджеру:</h1><table style='border: 1px solid #000'><thead><tr><th>ФИО</th><th>Телефон</th><th>Адрес доставки</th><th>Сумма заказа</th><th>Дата заказа</th><th>Статус</th></tr></thead><tbody><tr><td>${req.body.fullName}</td><td>${req.body.phone}</td><td>${req.body.address}</td><td>${req.body.price}грн.</td><td>${date}</td><td>В обработке</td></tr></tbody></table><p>Ожидайте наш менеджер свяжется с вами для подтверждения заказа </p>`;
 
       const text = 'Ваш заказ был принят';
 
--- src/routes/basket/basket-update-status.route.js
+++ src/routes/basket/basket-update-status.route.js
@@ -22,7 +22,9 @@ export default function (req, res) {
             break;
         }
 
-        const html = `<h1>Ваш заказ:</h1><table style='border: 1px solid #000'><thead><tr><th>ФИО</th><th>Телефон</th><th>Адрес доставки</th><th>Сумма заказа</th><th>Статус</th></tr></thead><tbody><tr><td>${basket.fullName}</td><td>${basket.phone}</td><td>${basket.address}</td><td>${basket.price}грн.</td><td>${status}</td></tr></tbody></table>`;
+        const date = new Date(req.body.orderDate).toLocaleDateString();
+
+        const html = `<h1>Ваш заказ:</h1><table style='border: 1px solid #000'><thead><tr><th>ФИО</th><th>Телефон</th><th>Адрес доставки</th><th>Сумма заказа</th><th>Дата заказа</th><th>Статус</th></tr></thead><tbody><tr><td>${basket.fullName}</td><td>${basket.phone}</td><td>${basket.address}</td><td>${basket.price}грн.</td><td>${date}</td><td>${status}</td></tr></tbody></table>`;
 
         mailer(html, text, req.body.email);
       }
